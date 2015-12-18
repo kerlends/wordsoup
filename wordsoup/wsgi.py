9b49f365956761b4c3741c7960056889e239e52e
kerlends <konrade@gmail.com>
2015-12-18 13:41:30 -0400

    fixes

--- wordsoup/wsgi.py
+++ wordsoup/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "anags.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wordsoup.settings")
 
 application = get_wsgi_application()
